0cce75f36e1235424dec4cb3e951aa9ff3e6867d
Andrew Greenwood <andrewgreenwoo@gmail.com>
2016-01-05 00:54:49 -0600

    switching info panels done

--- Assets/Scripts/InbetweenScreen.cs
+++ Assets/Scripts/InbetweenScreen.cs
@@ -12,11 +12,21 @@ public class InbetweenScreen : MonoBehaviour
 	public Text totalScoreText;
 	public Text highScoreText;
 	public Sprite[] infoPanels;
+	public GameObject infoHolder;
 
-	void Start ()
-	{
+	void Start() {
 		levelScoreText.text = PlayerPrefs.GetInt ("levelScore").ToString ();
 		highScoreText.text = PlayerPrefs.GetInt ("highScore").ToString ();
 		totalScoreText.text = PlayerPrefs.GetInt ("totalScore").ToString ();
+
+		int currentLev = PlayerPrefs.GetInt ("currentLevel") - 2;
+		if (currentLev > 9) {
+			currentLev -= 6;
+		} else if (currentLev > 18) {
+			currentLev -= 15;
+		} else if (currentLev > 27) {
+			currentLev -= 24;
+		}
+		infoHolder.GetComponent<SpriteRenderer>().sprite = infoPanels[currentLev];
 	}
 }
